36e5e574fd7f244feddfcfde9f9fffa3f526529f
Aman Tiwary <amantiwary161@gmail.com>
2026-06-10 11:59:38 +0530

    Time: 120 ms (22.86%), Space: 110.2 MB (14.95%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -1,38 +1,61 @@
 class Solution {
 public:
-    vector<vector<int>> v;
-    vector<int> counter, res;
+    long result_base_node = 0;
+    vector<int> count;
+    int N;
+    int dfsBase(unordered_map<int, vector<int>> &adj, int curr_node, int prev_node, int curr_depth) {
+        int total_node = 1;
         
-    void dfs(int i, int p = -1) {
-        for(auto u : v[i]) {
-            if(u == p) continue;
-            dfs(u, i);
-            counter[i] += counter[u];
-            res[i] += res[u] + counter[u];
+        result_base_node += curr_depth;
+        
+        for(int &child : adj[curr_node]) {
+            if(child == prev_node)
+                continue;
+            
+            total_node += dfsBase(adj, child, curr_node, curr_depth+1);
         }
-        counter[i] += 1;
+        
+        
+        count[curr_node] = total_node;
+        
+        return total_node;
     }
-
-    void dfs2(int i, int n, int p = -1) {
-        for(auto u : v[i]) {
-            if(u == p) continue;
-            res[u] = res[i] - counter[u] + n - counter[u];
-            dfs2(u, n, i);
+    
+    void DFS(unordered_map<int, vector<int>> &adj, int parent_node, int prev_node, vector<int>& result) {
+        
+        for(int &child : adj[parent_node]) {
+            if(child == prev_node)
+                continue;
+            
+            result[child] = result[parent_node] - count[child] + (N - count[child]);
+            DFS(adj, child, parent_node, result);
         }
+        
     }
     
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-        v.resize(n);
-        for(int i = 0; i < n - 1; i++) {
-            int a = edges[i][0];
-            int b = edges[i][1];
-            v[a].push_back(b);
-            v[b].push_back(a);
+        unordered_map<int, vector<int>> adj;
+        N = n;
+        count.resize(n, 0);
+        for(auto &vec : edges) {
+            int u = vec[0];
+            int v = vec[1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-        res.resize(n);
-        counter.resize(n);
-        dfs(0);
-        dfs2(0, n);
-        return res;
+        
+        
+        result_base_node = 0;
+        
+        dfsBase(adj, 0, -1, 0);
+        
+        vector<int> result(n, 0);
+        
+        result[0] = result_base_node;
+        
+        DFS(adj, 0, -1, result);
+        
+        return result;
     }
 };
