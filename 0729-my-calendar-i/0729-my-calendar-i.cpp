093dc1d96028561c3b182a9612278a61d3b2bb81
Aman Tiwary <amantiwary161@gmail.com>
2026-06-28 17:30:02 +0530

    Time: 7 ms (94.88%), Space: 43.5 MB (68.91%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,12 +1,29 @@
+struct Node{
+    int s, t;
+    Node *left, * right;
+    Node(int s, int t): s(s), t(t), left(NULL), right(NULL){}
+};
 class MyCalendar {
+    Node* root;
 public:
-    map<int,int>events;
-    MyCalendar() {   
+    MyCalendar(): root(NULL) {}
+    
+    bool dfs(int s, int t, Node* &curr){
+        if (!curr) {
+            curr=new Node(s, t);
+            return 1;
+        }
+        if (t<=curr->s) return dfs(s, t, curr->left);
+        else if (s>=curr->t) return dfs(s, t, curr->right);
+        else return 0;
     }
     bool book(int start, int end) {
-        auto next = events.upper_bound(start);
-        if(next != events.end() &&   (*next).second < end)return false;
-        events.insert({end,start});
-        return true;
+        return dfs(start, end, root);
     }
 };
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
