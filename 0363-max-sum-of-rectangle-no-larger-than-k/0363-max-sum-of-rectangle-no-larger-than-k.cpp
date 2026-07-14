db6355128923acb1a17f4a8ed443e8bfbeeb4324
Aman Tiwary <amantiwary161@gmail.com>
2026-07-15 01:23:11 +0530

    Time: 687 ms (61.04%), Space: 241.9 MB (34.14%) - LeetHub

--- 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
     int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
-         int m = matrix.size();
-        int n = matrix[0].size();
+        if (matrix.empty())
+            return 0;
 
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                dp[r + 1][c + 1] =
-                    matrix[r][c] + dp[r][c + 1] + dp[r + 1][c] - dp[r][c];
-            }
-        }
-
-        int count = 0;
-        int ans = INT_MIN;
-
-        for (int sR = 0; sR < m; sR++) {
-            for (int sC = 0; sC < n; sC++) {
-                for (int eR = sR; eR < m; eR++) {
-                    for (int eC = sC; eC < n; eC++) {
-
-                        int sum = dp[eR + 1][eC + 1] + dp[sR][sC] -
-                                  dp[eR + 1][sC] - dp[sR][eC + 1];
-
-                        if (sum <= k)
-                            ans = max(ans , sum);
-                    }
+        int row = matrix.size(), col = matrix[0].size(), res = INT_MIN;
+        auto func =
+            [&](vector<int>& sums) {
+                set<int> accuSet;
+                accuSet.insert(0);
+                int curSum = 0, curMax = INT_MIN;
+                for (int sum : sums) {
+                    curSum += sum;
+                    set<int>::iterator it = accuSet.lower_bound(curSum - k);
+                    if (it != accuSet.end())
+                        curMax = std::max(curMax, curSum - *it);
+                    accuSet.insert(curSum);
                 }
+                res = std::max(res, curMax);
+            };
+
+        for (int l = 0; l < col; ++l) {
+            vector<int> sums(row, 0);
+            for (int r = l; r < col; ++r) {
+                for (int i = 0; i < row; ++i) {
+                    sums[i] += matrix[i][r];
+                }
+
+               func(sums);
             }
         }
-
-        return ans;
+        return res;
     }
 };
